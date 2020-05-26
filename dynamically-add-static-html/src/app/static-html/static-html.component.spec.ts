b1e9a568a9635319347b5a6c182c217b6d3bdfe1
pawelkrzysztofostojski@gmail.com <pawelkrzysztofostojski@gmail.com>
2020-05-26 17:31:44 +0200

    dynamically add static html example

---
+++ dynamically-add-static-html/src/app/static-html/static-html.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StaticHtmlComponent } from './static-html.component';
+
+describe('StaticHtmlComponent', () => {
+  let component: StaticHtmlComponent;
+  let fixture: ComponentFixture<StaticHtmlComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ StaticHtmlComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StaticHtmlComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
